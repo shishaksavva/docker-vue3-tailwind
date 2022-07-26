e0dfbac2811cc4ce95fdba6a7bd7b111ce4bbbb2
savva <sav.shishak@gmail.com>
2022-07-26 20:30:25 +0300

    Update vue folder

---
+++ vue/src/components/ui/chip.vue
@@ -0,0 +1,34 @@
+<template>
+  <span
+    :class="`
+      text-grey
+      font-bold
+      text-xs
+      ${color}
+      rounded-[15px]
+      p-2
+      ml-1
+      flex
+      items-center
+    `"
+  >
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['color'],
+};
+</script>
+
+<style>
+.blue {
+  background: #065fd4;
+  color: white;
+}
+.green {
+  background: #00e676;
+  color: black;
+}
+</style>
